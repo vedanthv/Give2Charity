eb8294dcf4aebe581ae027d40d8066f8191eb7a0
vedanth <vedanthvbaliga@gmail.com>
2021-07-09 23:34:31 +0530

    added host to Procfile

--- app.js
+++ app.js
@@ -590,9 +590,19 @@ app.get("/reward", function(req, res) {
 
 });
 
+//port(as described above) and host are both wrong
+const host = 'localhost';
+const port = 3000;
+
+//use alternate localhost and the port Heroku assigns to $PORT
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+    console.log("Server started.......");
+});
 
 
-
-app.listen(process.env.PORT || 3006, function() {
-    console.log("server running on port 3006")
-})
+// app.listen(process.env.PORT || 3006, function() {
+//     console.log("server running on port 3006")
+// })
